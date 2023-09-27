47cd6980c1eb9dcefc72452012ea320db58b7496
virginie <virginie.grall@gmail.com>
2023-09-27 16:49:56 +0200

    connexion BDD

--- controlers/crepeControler.js
+++ controlers/crepeControler.js
@@ -1,2 +1,20 @@
+// import crepeCarte from "../public/javascript/crepeCarte.js";
+// export default (req, res) => res.render("crepeCarte", { crepeCarte });
+
+import query from "../database.js";
 import crepeCarte from "../public/javascript/crepeCarte.js";
-export default (req, res) => res.render("crepeCarte", { crepeCarte });
+
+export default (req, res) => {
+  query(
+    "SELECT UPPER(name) 'name', price FROM menuItems WHERE type = 'CRP'",
+    [],
+    (error, result) => {
+      if (error) {
+        console.error(`Erreur lors de l'exécution de la requête ${error}`);
+        res.status(500).send("Erreur serveur");
+        return;
+      }
+      res.render("crepeCarte", { crepeCarte: result });
+    }
+  );
+};
