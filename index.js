dbe19e025e3d29776af997f34b39ac73a5ce11c9
virginie <virginie.grall@gmail.com>
2023-09-27 12:31:00 +0200

    page-carte-menu

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 import express from "express";
 import router from "./router.js";
+import { crepeCarte } from "./public/javascript/crepeCarte.js"; // Importe les données de crepeCarte.js
 
 const PORT = 3000;
 const app = express();
 
-app.set("view engine", "ejs");
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.set("view engine", "ejs"); // Définit le moteur de modèle EJS
+app.set("views", "./views"); // Définit le répertoire des vues
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use(router);
+
+app.get("/crepe", (req, res) => {
+  res.render("crepeCarte", { crepeCarte });
+  console.log("la vue crepeCarte.ejs est renvoyée");
+});
 
 app.listen(PORT, () => {
   console.log(`Le serveur écoute sur : http://localhost:${PORT}`);
--- router.js
+++ router.js
@@ -1,10 +1,12 @@
 import express from "express";
 import homeControler from "./controlers/homeControler.js";
 import avisControler from "./controlers/avisControler.js";
+import crepeControler from "./controlers/crepeControler.js";
 
 const router = express.Router();
 
 router.get("/", homeControler);
 router.get("/avis", avisControler);
+router.get("/crepe", crepeControler);
 
 export default router;
