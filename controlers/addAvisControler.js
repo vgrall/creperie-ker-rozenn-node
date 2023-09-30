6c3c722ed5fb16515591f34d8a4a4eeb88f63a32
virginie <virginie.grall@gmail.com>
2023-09-30 12:19:56 +0200

    peaufinage avis et slider

--- controlers/addAvisControler.js
+++ controlers/addAvisControler.js
@@ -1,11 +1,12 @@
 import query from "../database.js";
 import { v4 } from "uuid";
-import formidable from "formidable";
 
 export default (req, res) => {
   console.log("POST /avis");
   console.log(req.body);
 
+  let photo = req.body.photo;
+  console.log("la photo : ", photo);
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
@@ -13,9 +14,14 @@ export default (req, res) => {
   const satisfaction = req.body.satisfaction;
   const id = v4();
 
+  // tester si le champ photo est vide et le mettre à undifined
+  if (photo.trim() == "") {
+    photo = "/images/avatarForm.png";
+  }
+
   query(
-    "INSERT INTO AVIS_CLIENTS (id, firstName, lastName, email, comment, satisfaction) VALUES (?, ?, ?, ?, ?, ?)",
-    [id, firstName, lastName, email, comment, satisfaction],
+    "INSERT INTO AVIS_CLIENTS (id, firstName, lastName, email, comment, satisfaction, photo) VALUES (?, ?, ?, ?, ?, ?, ?)",
+    [id, firstName, lastName, email, comment, satisfaction, photo],
     (error, result) => {
       if (error) {
         console.error(`Erreur lors de l'exécution de la requête ${error}`);
